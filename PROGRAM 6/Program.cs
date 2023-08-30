47dd37173019f716a8bc2f5361830277399b7095
user <user@DESKTOP-3C631U4>
2023-08-30 12:20:33 +0530

    Add project files.

---
+++ PROGRAM 6/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter 5 Elements");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                arr[i] = arr[i] * 2;
+            }
+
+
+            Console.WriteLine("Array is :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
